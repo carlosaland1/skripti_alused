c9342bd80e386e25f485c7160d3b896a46463186
Carlos Aland <carlos.aland@khk.ee>
2020-11-18 13:22:59 +0200

    muutujatega lause moodustamise test

---
+++ praks3/yl2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# muutujatega moodustatud lause
+# autor - Carlos Aland
+#
+$aasta = "2016"
+$president = "Kersti Kaljulaid"
+$lause_keskosa = ". aastal valiti Eesti presidendiks "
+$lause = $aasta $lause_keskosa $president
+echo "$lause" 
