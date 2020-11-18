778f2ce311daba80286aa4d4f668116951568082
Carlos Aland <carlos.aland@khk.ee>
2020-11-18 13:14:33 +0200

    tervituse lisamine

---
+++ praks3/yl1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# kasutaja eesnime ja perenime küsimine ning tervitus
+# autor - Carlos Aland
+#
+echo -n "Sisesta oma eesnimi ja perenimi: "
+read eesnimi perenimi
+echo "Tere tulemast, $eesnimi $perenimi"
