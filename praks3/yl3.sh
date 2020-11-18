1bcb50950588a35406719b2ed0744135c86cbb54
Carlos Aland <carlos.aland@khk.ee>
2020-11-18 14:03:51 +0200

    muutujate kontroll

---
+++ praks3/yl3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# nädala eeldatav ajakulu
+# autor - Carlos Aland
+#
+echo -n "Siestage  ainepunktide arv: "
+read ainepunktid
+echo "Siesestage nädalate arv: "
+read nädalate_arv
+echo "Ainepunktid on $ainepunktid ja nädalate arv on $nädalate_arv"
