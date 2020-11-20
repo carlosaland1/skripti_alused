6de54bc5e35b446622b5182215bfb1bc71ceb735
Carlos Aland <carlos.aland@khk.ee>
2020-11-20 11:31:47 +0200

    kuu numbri abil väljastatakse kuu nimetus

---
+++ praks6/yl3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# kuu numbrile tuleb vastav kuu nimetus
+# autor - Carlos Aland
+#
+echo "Sisesta kuu number: "
+read number
+case $number in
+   "1") echo "Jaanuar";;
+   "2") echo "Veebruar";;
+   "3") echo "Märts";;
+   "4") echo "Aprill";;
+   "5") echo "Mai";;
+   "6") echo "Juuni";;
+   "7") echo "Juuli";;
+   "8") echo "August";;
+   "9") echo "September";;
+   "10") echo "Oktoober";;
+   "11") echo "November";;
+   "12") echo "Detsember";;
+   *) echo "Sorry, I can not get a $rental for you";;
+esac
+
