3ae23cd8d363f7491dfedbc8192b4439522891e2
Carlos Aland <carlos.aland@khk.ee>
2020-11-20 11:45:27 +0200

    algarvu tuvastamine parandus

--- praks6/yl4.sh
+++ praks6/yl4.sh
@@ -3,7 +3,7 @@
 # algarvu tuvastamine
 # autor - Carlos Aland
 #
-echo -e "Sisesta suvaline arv: "
+echo -n "Sisesta suvaline arv: "
 read arv
 if [ $arv -eq 0 -o $arv -eq 1 ]; then
 	echo "$arv - ei ole algarv"
@@ -15,7 +15,7 @@ else
 		jagaja=$(($jagaja + 1))
 		j22k=$(($arv % $jagaja))
 	done
-	id [ $arv -eq $jagaja ]; then
+	if [ $arv -eq $jagaja ]; then
 		echo "$arv - on algarv"
 	else
 		echo "$arv - ei ole algarv"
