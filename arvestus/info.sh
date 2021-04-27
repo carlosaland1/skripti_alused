a1849bc8c786ebc52c40470779dbf944313b22e1
Carlos Aland <carlos.aland@khk.ee>
2021-04-28 01:54:39 +0300

    kasutaja tervitus ning kuupäeva väljastus

---
+++ arvestus/info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#kasutaja tervitus ning kuupäeva väljastus, nime muutus
+#autor - Carlos Aland
+
+echo -n  "Tere, "
+echo -n $(whoami)
+echo "!"
+echo -n "Täna on "
+echo $(date +"%x")
+echo -n "Kell on "
+echo $(date +%H:%M)
+
